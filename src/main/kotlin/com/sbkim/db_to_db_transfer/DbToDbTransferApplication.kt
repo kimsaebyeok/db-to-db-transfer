f3e954e7c50c27a3d1eae5d556ea40db39f9ff6c
sbkim-muhayu <sbkim@muhayu.com>
2024-06-07 17:21:18 +0900

    최초 커밋

---
+++ src/main/kotlin/com/sbkim/db_to_db_transfer/DbToDbTransferApplication.kt
@@ -0,0 +1,11 @@
+package com.sbkim.db_to_db_transfer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DbToDbTransferApplication
+
+fun main(args: Array<String>) {
+	runApplication<DbToDbTransferApplication>(*args)
+}
---
+++ src/test/kotlin/com/sbkim/db_to_db_transfer/DbToDbTransferApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sbkim.db_to_db_transfer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DbToDbTransferApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
